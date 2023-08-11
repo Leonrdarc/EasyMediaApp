8de71f658a982a57007458da5267bf60b005631f
Leonardo Archila <leonardoarchila@hotmail.com>
2023-08-11 16:21:40 -0500

    🐛 fix(backend): fix bug in JWT encoding for user register

--- back/src/controllers/userController.ts
+++ back/src/controllers/userController.ts
@@ -10,7 +10,11 @@ import { AppError } from "../middlewares/errorHandler";
  * @param req - Express request object.
  * @param res - Express response object.
  */
-export const registerUser = async (req: Request, res: Response, next: NextFunction) => {
+export const registerUser = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
   try {
     const { name, email, password } = req.body;
 
@@ -32,15 +36,14 @@ export const registerUser = async (req: Request, res: Response, next: NextFuncti
 
     await user.save();
 
-    const token = encodeJWT(user);
+    const userJSON = user.toJSON();
+    const token = encodeJWT(userJSON);
 
     // Respond to the client
     res.status(201).json({
       message: "User created successfully.",
-      data: {
-        user,
-        token,
-      },
+      user: userJSON,
+      token,
     });
   } catch (error) {
     return next(error);
@@ -77,7 +80,7 @@ export const loginUser = async (
       // Respond to the client with the user and the token
       res.status(201).json({
         message: "User loged in successfully.",
-        userJSON,
+        user: userJSON,
         token,
       });
     } else {
