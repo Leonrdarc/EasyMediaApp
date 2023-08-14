029e53f9c3c737a40a67e8984bd32536d5ca35f7
Leonardo Archila <leonardoarchila@hotmail.com>
2023-08-14 16:09:31 -0500

    🔧 chore(frontend): add size input property to button for dynamic sizing

--- front/src/app/shared/components/button/button.component.ts
+++ front/src/app/shared/components/button/button.component.ts
@@ -3,9 +3,10 @@ import { Component, Input } from '@angular/core';
 @Component({
   selector: 'app-button',
   templateUrl: './button.component.html',
-  styleUrls: ['./button.component.scss']
+  styleUrls: ['./button.component.scss'],
 })
 export class ButtonComponent {
   @Input() block: boolean = false;
   @Input() disabled: boolean = false;
+  @Input() size: 'small' | 'large' = 'large';
 }
