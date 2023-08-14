8725bf175112bfd83ae46759d02c1c1267ddd4a6
Leonardo Archila <leonardoarchila@hotmail.com>
2023-08-14 01:57:02 -0500

    ✨ feat(frontend): update input component to use FormControl

--- front/src/app/shared/components/input/input.component.ts
+++ front/src/app/shared/components/input/input.component.ts
@@ -1,49 +1,19 @@
-import { Component, Input, forwardRef } from '@angular/core';
-import { NG_VALIDATORS, NG_VALUE_ACCESSOR, NgModel } from '@angular/forms';
+import { Component, Input } from '@angular/core';
+import { FormControl } from '@angular/forms';
 
 @Component({
-  selector: 'app-input-toggle',
+  selector: 'app-input',
   templateUrl: './input.component.html',
-  styleUrls: ['./input.component.scss'],
-  providers: [
-    {
-      provide: NG_VALUE_ACCESSOR,
-      useExisting: forwardRef(() => InputComponent),
-      multi: true
-    },
-    {
-      provide: NG_VALIDATORS,
-      useExisting: forwardRef(() => InputComponent),
-      multi: true
-    }
-  ]
+  styleUrls: ['./input.component.scss']
 })
-
 export class InputComponent {
-  
-  @Input() type: 'text' | 'password' = 'text';
-  @Input() label: string = '';
-  @Input() name: string = '';
-  @Input() required: boolean = false;
+  @Input() label?: string;
+  @Input() placeholder?: string = '';  // Default to an empty string if not provided
+  @Input() type: string = 'text'; // default to text if not provided
+  @Input() control!: FormControl;
+  showPassword: boolean = false;
 
-  inputValue: string = '';
-  inputType: 'text' | 'password' = 'text';
-
-  get isPassword(): boolean {
-    return this.type === 'password';
+  togglePasswordVisibility() {
+    this.showPassword = !this.showPassword;
   }
-
-  ngOnInit() {
-    this.inputType = this.type;
-  }
-
-  toggleInputType() {
-    if (this.inputType === 'password') {
-      this.inputType = 'text';
-    } else {
-      this.inputType = 'password';
-    }
-  }
-
 }
-
