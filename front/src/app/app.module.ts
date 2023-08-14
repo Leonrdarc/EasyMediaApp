1226e78fa04feb3c0360dedb44d238254fe32dff
Leonardo Archila <leonardoarchila@hotmail.com>
2023-08-14 16:11:27 -0500

    ✨ feat(frontend): add new components + pipes + interceptors to app module

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
 import { ReactiveFormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -15,7 +15,17 @@ import { NavBarComponent } from './shared/components/nav-bar/nav-bar.component';
 import { ButtonComponent } from './shared/components/button/button.component';
 import { InputComponent } from './shared/components/input/input.component';
 import { NavMenuComponent } from './shared/components/nav-bar/nav-menu/nav-menu.component';
+import { PostCardComponent } from './shared/components/post-card/post-card.component';
 import { AuthService } from './core/services/auth.service';
+import { APIInterceptor } from './core/interceptors/api.interceptor';
+import { TextFieldComponent } from './shared/components/text-field/text-field.component';
+import { DatePipe } from '@angular/common';
+import { ModalComponent } from './shared/components/modal/modal.component';
+import { HighlightPipe } from './shared/pipes/highlight.pipe';
+import { DaySelectorComponent } from './shared/components/day-selector/day-selector.component';
+import { EmptyComponent } from './shared/components/empty/empty.component';
+import { SpinnerComponent } from './shared/components/spinner/spinner.component';
+import { PaginatorComponent } from './shared/components/paginator/paginator.component';
 @NgModule({
   declarations: [
     AppComponent,
@@ -28,6 +38,14 @@ import { AuthService } from './core/services/auth.service';
     ButtonComponent,
     InputComponent,
     NavMenuComponent,
+    PostCardComponent,
+    TextFieldComponent,
+    ModalComponent,
+    HighlightPipe,
+    DaySelectorComponent,
+    EmptyComponent,
+    SpinnerComponent,
+    PaginatorComponent,
   ],
   imports: [
     BrowserModule,
@@ -36,7 +54,15 @@ import { AuthService } from './core/services/auth.service';
     ReactiveFormsModule,
     HttpClientModule,
   ],
-  providers: [AuthService],
+  providers: [
+    AuthService,
+    DatePipe,
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: APIInterceptor,
+      multi: true,
+    },
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
