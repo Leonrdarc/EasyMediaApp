a8d1070e28a2c43bae96d38e4b601d800d78910c
Leonardo Archila <leonardoarchila@hotmail.com>
2023-08-14 16:08:37 -0500

    🎨 style(frontend): update nav-bar component html and styling

--- front/src/app/shared/components/nav-bar/nav-menu/nav-menu.component.ts
+++ front/src/app/shared/components/nav-bar/nav-menu/nav-menu.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/core/services/auth.service';
 
 @Component({
   selector: 'app-nav-menu',
   templateUrl: './nav-menu.component.html',
-  styleUrls: ['./nav-menu.component.scss']
+  styleUrls: ['./nav-menu.component.scss'],
 })
 export class NavMenuComponent {
+  constructor(private router: Router, private authService: AuthService) {}
 
+  user = JSON.parse(localStorage.getItem('user') ?? '{}');
+  isExpanded = false;
+
+  toggle() {
+    this.isExpanded = !this.isExpanded;
+  }
+
+  logout() {
+    this.authService.logout();
+    this.router.navigateByUrl('/sign-in');
+  }
 }
